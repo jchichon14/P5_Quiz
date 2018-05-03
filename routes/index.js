357bddc1c920f067b52dc44913836c3b9dab0bd6
Julian <jchichon14@gmail.com>
2018-05-03 20:02:42 +0200

    version1

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/credits', function(req, res, next) {
+  res.render('credits');
+});
+
+router.get('/quizzes', function(req, res, next) {
+  res.render('quizzes');
+});
+
+module.exports = router;
